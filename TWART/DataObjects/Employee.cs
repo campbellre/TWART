616d73d4d86f9a023b1a8cab4d32fcd332b4bf31
campbellre <campbellre@outlook.com>
2015-11-03 10:47:03 +0000

    extra ;

--- TWART/DataObjects/Employee.cs
+++ TWART/DataObjects/Employee.cs
@@ -7,16 +7,16 @@ namespace TWART.Models
 {
     public class Employee
     {
-        private int Id { get; set; };
-        private string Firstname { get; set; };
-        private string Lastname { get; set; };
-        private DateTime Dob { get; set; };
-        private string ContactNumber { get; set; };
-        private DateTime Startdate { get; set; };
-        private DateTime EndDate { get; set; };
-        private int Dept { get; set; };
-        private int Depot { get; set; };
-        private int Role { get; set; };
+        private int Id { get; set; }
+        private string Firstname { get; set; }
+        private string Lastname { get; set; }
+        private DateTime Dob { get; set; }
+        private string ContactNumber { get; set; }
+        private DateTime Startdate { get; set; }
+        private DateTime EndDate { get; set; }
+        private int Dept { get; set; }
+        private int Depot { get; set; }
+        private int Role { get; set; }
 
 
     }
