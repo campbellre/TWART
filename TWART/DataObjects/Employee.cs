8e76144a36b3034f8511f56c0210ee5448b0bfa4
campbellre <campbellre@outlook.com>
2015-11-03 17:49:24 +0000

    Add DataObjects.

--- TWART/DataObjects/Employee.cs
+++ TWART/DataObjects/Employee.cs
@@ -1,22 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 
-namespace TWART.Models
+namespace TWART.DataObjects
 {
     public class Employee
     {
-        private int Id { get; set; }
-        private string Firstname { get; set; }
-        private string Lastname { get; set; }
-        private DateTime Dob { get; set; }
-        private string ContactNumber { get; set; }
-        private DateTime Startdate { get; set; }
-        private DateTime EndDate { get; set; }
-        private int Dept { get; set; }
-        private int Depot { get; set; }
-        private int Role { get; set; }
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public DateTime Dob { get; set; }
+        public string ContactNumber { get; set; }
+        public DateTime Startdate { get; set; }
+        public DateTime EndDate { get; set; }
+        public int Dept { get; set; }
+        public int Depot { get; set; }
+        public int Role { get; set; }
 
 
     }
---
+++ TWART/DataObjects/Role.cs
@@ -0,0 +1,8 @@
+﻿namespace TWART.DataObjects
+{
+    public class Role
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } 
+    }
+}
