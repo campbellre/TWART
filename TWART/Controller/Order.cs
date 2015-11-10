7730f1d9a57e3d286446ae8e383d49ccf1520e1f
campbellre <campbellre92@outlook.com>
2015-11-10 14:06:53 +0000

    Added Additional files

---
+++ TWART/Controller/Order.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TWART.Controller
+{
+    // Handles the setting up of an order
+    public class Order
+    {
+        // Class parameters
+        int orderID;
+        int accountID;
+        int clientID;
+        DateTime placed;
+
+        // Class constructor
+        public Order()
+        {
+            // CONSTRUCTOR GOES HERE
+        }
+    }
+}
