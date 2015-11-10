147819510e9a1c4cb425205c7d4c726f77b7ae3a
Andrew Davis <asutherlandavis@googlemail.com>
2015-11-10 16:21:11 +0000

    More controllers
    Added some Edit Details controller classes.
    Removed irrelevant people from a photo.

---
+++ TWART/Controller/EditContactDetails.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TWART.Controller
+{
+    // Handles the login to the server / database
+    public class EditContactDetails
+    {
+        // Class parameters - THERE'S PROBABLY MORE
+        int contactId;
+        String telephone;
+        String firstName;
+        String surname;
+
+
+        // Constructor
+        public EditContactDetails (int ID, String tele, String fN, String sN)
+        {
+            // CONSTRUCTOR GOES HERE
+            this.contactId = ID;
+            this.telephone = tele;
+            this.firstName = fN;
+            this.surname = sN;
+        }
+
+        // Blank constructor
+        public EditContactDetails() 
+        { 
+            // Blank constructor    
+        }
+
+        /*
+         * Handles the process to edit details for a user
+         * 
+         * @param: 
+         * @return: If successful
+         */
+        public bool saveDetails()
+        {
+            //return ryansMethod(this.contactId, this.telephone, this.firstName, this.surname);
+            return false;
+        }
+    }
+}
---
+++ TWART/Controller/EditEmployeeDetails.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TWART.Controller
+{
+    // Handles the login to the server / database
+    public class EditEmployeeDetails
+    {
+        // Class parameters - THERE'S PROBABLY MORE
+        int employeeId;
+        String contactNumber;
+        String firstName;
+        String surname;
+        Date DOB;
+        Date start_Date;
+        Date end_Date;
+        int depot_id;
+        int role_id;
+
+
+        // Constructor
+        public EditEmployeeDetails (int ID, String contact, String fN, String sN, Date bDate, Date sDate, Date eDate, int depot, int role)
+        {
+            // CONSTRUCTOR GOES HERE
+            this.employeeId = ID;
+            this.contactNumber = tele;
+            this.firstName = fN;
+            this.surname = sN;
+            this.DOB = bDate;
+            this.start_date = sDate;
+            this.end_date = eDate;
+            this.depot_id = depot;
+            this.role_id = role;
+
+        }
+
+        // Blank constructor
+        public EditEmployeeDetails() 
+        { 
+            // Blank constructor    
+        }
+
+        /*
+         * Handles the process to edit details for a user
+         * 
+         * @param: 
+         * @return: If successful
+         */
+        public bool saveDetails()
+        {
+            //return ryansMethod(this.contactId, this.telephone, this.firstName, this.surname);
+            return false;
+        }
+    }
+}
