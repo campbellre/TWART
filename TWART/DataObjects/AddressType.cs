dbf830e5e99c9b77f692f5d982d0ef0c5d323182
campbellre <campbellre92@outlook.com>
2015-11-11 17:05:11 +0000

    Added Comments to the Enums.

--- TWART/DataObjects/AddressType.cs
+++ TWART/DataObjects/AddressType.cs
@@ -1,6 +1,8 @@
 ﻿namespace TWART.DataObjects
 {
-    // This is what comments are for.
+    // This is a Enum for that allow the type of address to be passes for searching.
+    // Source - The source address for a package in the database.
+    // Destination - The destination address for a package in the database.
     public enum AddressType
     {
         Source,
--- TWART/DataObjects/PackageDateType.cs
+++ TWART/DataObjects/PackageDateType.cs
@@ -1,5 +1,9 @@
 ﻿namespace TWART.DataObjects
 {
+    // This is a Enum for specifiy when you want to search for a package.
+    // Places - The date the package was placed.
+    // Received - The date that we (the company) recived the package from the customer.
+    // Delivered - The date we plan delivering or the date we did deliver the package for the customer.
     public enum PackageDateType
     {
         Placed,
