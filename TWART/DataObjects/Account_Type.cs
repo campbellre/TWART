8e544b12a9dba874caf1e02dcb47673f94303edf
campbellre <campbellre92@outlook.com>
2015-11-11 13:15:26 +0000

    Added Properties
    Have Added the Properties to the Account_Type and bank DataObject.

--- TWART/DataObjects/Account_Type.cs
+++ TWART/DataObjects/Account_Type.cs
@@ -7,5 +7,10 @@ namespace TWART.DataObjects
 {
     public class Account_Type
     {
+        public int ID { get; set; }
+        public String Name { get; set; }
+        public String Benefit { get; set; }
+        public Decimal Cost { get; set; }
+    }
     }
 }
--- TWART/DataObjects/Bank.cs
+++ TWART/DataObjects/Bank.cs
@@ -7,5 +7,10 @@ namespace TWART.DataObjects
 {
     public class Bank
     {
+        public int ID { get; set; }
+        public Address Address { get; set; }
+        public String SortCode { get; set; }
+        public int AccountNumber { get; set; }
+        public Customer Customer { get; set; }
     }
 }
