179be1df389833512f18af494495465ac5464264
campbellre <campbellre92@outlook.com>
2015-11-11 14:14:07 +0000

    Added DataObjects
    Have Added the DataObject Transation methods.

---
+++ TWART/DataObjects/Transaction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TWART.Controller;
+
+namespace TWART.DataObjects
+{
+    public class Transaction
+    {
+        public int ID { get; set; }
+        public DateTime DateOfOrder { get; set; }
+        public Order Order { get; set; }
+        public Customer Customer { get; set; }
+        public Bank Bank { get; set; }
+
+    }
+}
