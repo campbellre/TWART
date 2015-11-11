87fd2410e97a8fb059a84f724d4384f1df080ee0
campbellre <campbellre92@outlook.com>
2015-11-11 14:15:07 +0000

    Added Model methods.
    Changed Name for Client model to Customer, to keep things the same.
    Have added methods to the Customer and Transation Models.

---
+++ TWART/Models/CustomerModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MySql.Data.MySqlClient;
+using TWART.DataObjects;
+using TWART.Properties;
+
+namespace TWART.Models
+{
+    public class CustomerModel
+    {
+        private MySqlConnection connect;
+        private string _connectionString;
+        
+        public CustomerModel()
+        {
+            _connectionString = Resource1.ConnectionString;
+        }
+
+        public void CreateCustomer(Customer c)
+        {
+            throw new NotImplementedException();
+        }
+
+        // Main method for getting a customer
+        public Customer SearchCustomers(int ID)
+        {
+            throw new NotImplementedException();
+        }
+
+        // Calls main method for getting a customer
+        public Customer SearchCustomers(Customer c)
+        {
+            return SearchCustomers(c.ID);
+        }
+
+        // Finds customers that have the deatils specified in the address object.
+        public List<Customer> SearchCustomers(Address a)
+        {
+            throw new NotImplementedException();
+        }
+
+        // Changes the customer specifed by the customer object account type to the Account_Type object.
+        public void ChangeAccountType(Customer c, Account_Type type)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
--- TWART/Models/TransactionModel.cs
+++ TWART/Models/TransactionModel.cs
@@ -2,10 +2,69 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using MySql.Data.MySqlClient;
+using TWART.Controller;
+using TWART.DataObjects;
+using TWART.Properties;
 
 namespace TWART.Models
 {
     public class TransactionModel
     {
+        private MySqlConnection connect;
+        private string _connectionString;
+
+        public TransactionModel()
+        {
+            _connectionString = Resource1.ConnectionString;
+        }
+
+        public void CreateTransaction(Transaction t)
+        {
+            throw new NotImplementedException();
+        }
+
+        // Main method for getting an transaction.
+        public Transaction SearchTransaction(int ID)
+        {
+            throw new NotImplementedException();
+        }
+
+        // Calls the main method for getting a transtion.
+        public Transaction SearchTransaction(Transaction t)
+        {
+            return SearchTransaction(t.ID);
+        }
+
+        // Returns Transations with the date of order specified by the datetime.
+        public List<Transaction> SearchTransactions(DateTime date)
+        {
+            throw new NotImplementedException();
+        }
+
+        // Returns Transations between the start and end datetime objects.
+        public List<Transaction> SearchTransactions(DateTime start, DateTime end)
+        {
+            throw new NotImplementedException();
+        }
+
+        // Finds Transactions with the specified details in the order object.
+        public List<Transaction> SearchTransactions(Order o)
+        {
+            throw new NotImplementedException();
+        }
+
+        // Finds Transations with the specifed details in the customer object.
+        public List<Transaction> SearchTransactions(Customer c)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        // Finds Transactions with the specified details in the bank object.
+        public List<Transaction> SearchTransactions(Bank b)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
