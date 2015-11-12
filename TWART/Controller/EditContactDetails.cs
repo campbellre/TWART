b3aed291dbd4fa83270bbde59eb2682b6caafe31
unknown <tristanhaley@labpc234.computing.dundee.ac.uk>
2015-11-12 12:59:47 +0000

    Made minor modifications to createOrder class.

--- TWART/Controller/EditContactDetails.cs
+++ TWART/Controller/EditContactDetails.cs
@@ -42,8 +42,8 @@ namespace TWART.Controller
          */
         public bool saveDetails()
         {
-            Contact newContact = new Contact(contactId, firstName, surname, position, telephone)
-            //return ryansMethod(newContact);
+            // Contact newContact = new Contact(contactId, firstName, surname, position, telephone);
+            // return ryansMethod(newContact);
             return false;
         }
     }
