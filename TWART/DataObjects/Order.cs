a7c48b8e34bd8146df0edb037e3aea1199090393
campbellre <campbellre92@outlook.com>
2015-11-17 15:56:52 +0000

    add order

---
+++ TWART/DataObjects/Order.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TWART.DataObjects
+{
+    public class Order
+    {
+        public int ID { get; set; }
+        public String Status { get; set; }
+        public DateTime DatePlaces { get; set; }
+        public DateTime DateReceived { get; set; }
+        public DateTime DateDelivered { get; set; }
+        public 
+    
+    }
+}
