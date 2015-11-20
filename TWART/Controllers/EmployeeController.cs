c55b2c7c4fb07c2242ed9962cfdf2cd8abc8926b
Budds2k6 <Budds_2k6@hotmail.co.uk>
2015-11-20 14:23:47 +0000

    Added try / catch for listing employees.

--- TWART/Controllers/EmployeeController.cs
+++ TWART/Controllers/EmployeeController.cs
@@ -145,23 +145,30 @@ namespace TWART.Controllers
                 var roleModel = new RoleModel();
                 var depotModel = new DepotModel();
 
-                // Gets the complete list
-                var el = employeeModel.GetEmployeesList();
-
-                // Attaches associated department / role to employee
-                foreach (var employee in el)
+                try
                 {
-                    Depot depot = depotModel.SearchDepot(employee.Depot);
-                    Department dept = departmentModel.SearchDepartment(employee.Dept);
-                    Role role = roleModel.SearchRoles(employee.Role);
+                    // Gets the complete list
+                    var el = employeeModel.GetEmployeesList();
 
-                    employee.DepotO = depot;
-                    employee.Department = dept;
-                    employee.RoleO = role;
+                    // Attaches associated department / role to employee
+                    foreach (var employee in el)
+                    {
+                        Depot depot = depotModel.SearchDepot(employee.Depot);
+                        Department dept = departmentModel.SearchDepartment(employee.Dept);
+                        Role role = roleModel.SearchRoles(employee.Role);
+
+                        employee.DepotO = depot;
+                        employee.Department = dept;
+                        employee.RoleO = role;
+                    }
+
+                    // Returns the list
+                    return View(el);
                 }
-
-                // Returns the list
-                return View(el);
+                catch
+                {
+                    return Redirect("/403.html");
+                } 
             }
             else
             {
