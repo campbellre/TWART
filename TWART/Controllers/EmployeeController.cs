af30950762bc51b6ddeb3057b2129193376427e9
Budds2k6 <Budds_2k6@hotmail.co.uk>
2015-11-20 01:37:43 +0000

    Added EmployeeController controller for controlling the employee controls.

---
+++ TWART/Controllers/EmployeeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TWART.DataObjects;
+using TWART.Models;
+
+namespace TWART.Controllers
+{
+    public class EmployeeController : System.Web.Mvc.Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            // Ensures logged in
+            if (Session["loggedInState"] == null)
+            {
+                return Redirect("/403.html");
+            }
+
+            // Checks if logged in
+            bool state = (bool)Session["loggedInState"];
+            if (state == true)
+            {
+                // Establishes employee model
+                EmployeesModel employeeModel = new EmployeesModel();
+
+                // Holds the new employee
+                Employee newEmployee = new Employee();
+
+                // Stored details for the employee
+                //newEmployee.
+
+
+                // Return created department to view
+                return View();
+            }
+            else
+            {
+                // If not logged in
+                return Redirect("/login.html");
+            }
+        }
+    }
+}
