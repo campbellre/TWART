4a18beb505254488e00b86261f633fda23e29f0c
Budds2k6 <Budds_2k6@hotmail.co.uk>
2015-11-20 14:17:26 +0000

    Added DepotController. Modified EmployeeContoller to link for full employee details. Updated Employee to contain null object reference for searching.

---
+++ TWART/Controllers/DepotController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TWART.DataObjects;
+using TWART.Models;
+
+namespace TWART.Controllers
+{
+    public class DepotController : System.Web.Mvc.Controller
+    {
+        // GET: Depot
+        public ActionResult Index()
+        {
+            // Ensures logged in
+            if (Session["loggedInState"] == null)
+            {
+                return Redirect("/403.html");
+            }
+
+            // Checks if logged in
+            bool state = (bool)Session["loggedInState"];
+            if (state == true)
+            {
+                // Establishes depot model
+                DepotModel depotModel = new DepotModel();
+
+                // Holds the new depot
+                Depot newDepot = new Depot();
+
+                // Stored details for the depot
+                newDepot.AddressID = int.Parse(Request.Form[0]);
+                newDepot.ManagerID = int.Parse(Request.Form[1]);
+                newDepot.DepotName = Request.Form[2];
+                newDepot.FloorSpace = Double.Parse(Request.Form[3]);
+                newDepot.NumVehicles = int.Parse(Request.Form[4]);
+
+                // Adds the object to the database
+                depotModel.CreateDepot(newDepot);
+
+                // Return created department to view
+                return View(newDepot);
+            }
+            else
+            {
+                // If not logged in
+                return Redirect("/login.html");
+            }
+        }
+    }
+}
