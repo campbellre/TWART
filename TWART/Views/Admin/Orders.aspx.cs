8d76a06fbb0c18cabe38e8a5a1e2e6fbff7dee74
campbellre <campbellre92@outlook.com>
2015-11-20 02:56:19 +0000

    Fix Order view.

--- TWART/Views/Admin/Orders.aspx.cs
+++ TWART/Views/Admin/Orders.aspx.cs
@@ -4,10 +4,11 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using TWART.DataObjects;
 
 namespace TWART.Views.Admin
 {
-    public partial class OrderView : System.Web.Mvc.ViewPage<IEnumerable<OrderView>>
+    public partial class OrderView : System.Web.Mvc.ViewPage<IEnumerable<Order>>
     {
         protected void Page_Load(object sender, EventArgs e)
         {
