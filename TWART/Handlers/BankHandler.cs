0f5800a6da8e4fea90dda330e94b6dab852a9935
Budds2k6 <Budds_2k6@hotmail.co.uk>
2015-11-21 20:24:23 +0000

    Added Handlers namespace. Re-worked a bunch of stuff. I can't even.

---
+++ TWART/Handlers/BankHandler.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TWART.Models;
+using TWART.DataObjects;
+
+namespace TWART.Handlers
+{
+    public class BankHandler
+    {
+        // Constructor
+        public BankHandler()
+        {}
+
+        // Creates a bank
+        public int create(int addressID, String sortCode, int accountNumber)
+        {
+            // Establishes model
+            BankingModel bankModel = new BankingModel();
+
+            // Holds the new bank
+            Bank newBank = new Bank();
+
+            // Stored details for the bank
+            newBank.Address_ID = addressID;
+            newBank.SortCode = sortCode;
+            newBank.AccountNumber = accountNumber;
+
+            // Creates the bank
+            int bankID = bankModel.CreateBank(newBank);
+
+            // Returns created bank
+            return bankID;
+        }
+
+        //// Returns a list of all banks
+        //public ActionResult GetBanks()
+        //{
+        //    // Null handling
+        //    if (Session["loggedInState"] == null)
+        //    {
+        //        return Redirect("/403.html");
+        //    }
+
+        //    // If logged in
+        //    bool state = (bool)Session["loggedInState"];
+        //    if (state == true)
+        //    {
+        //        // Creates models
+        //        var bankModel = new BankingModel();
+        //        var addressModel = new AddressModel();
+                
+        //        // Gets the complete list
+        //        var bl = bankModel.ListBanking();
+
+        //        if (bl.Count != 0)
+        //        {
+        //            // Attaches associated department / role to employee
+        //            foreach (var bank in bl)
+        //            {
+        //                // Acquires, and adds the bank address
+        //                Address address = null;
+        //                if (bank.Address_ID != 0)
+        //                {
+        //                    address = addressModel.SearchAddress(bank.Address_ID);
+        //                }
+
+        //                // Appends object to bank
+        //                bank.Address = address;
+        //            }
+
+        //            // Returns the list
+        //            return View(bl);
+        //        }
+        //        else
+        //        {
+        //            return Redirect("/403.html");
+        //        }
+        //    }
+        //    else
+        //    {
+        //        // If not logged in
+        //        return Redirect("/login.html");
+        //    }
+        //}
+    }
+}
---
+++ TWART/Handlers/CustomerHandler.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TWART.Models;
+using TWART.DataObjects;
+
+namespace TWART.Handlers
+{
+    public class CustomerHandler
+    {
+        // Class constructor
+        public CustomerHandler()
+        { }
+       
+        //// GET: Customer 
+        //public ActionResult GetCustomers()
+        //{
+        //    // If logged in
+        //    bool state = (bool)Session["loggedInState"];
+        //    if (state == true)
+        //    {
+        //        // Creates models
+        //        var addressModel = new AddressModel();
+        //        var customerModel = new CustomerModel();
+
+        //        // Gets the complete list
+        //        var customerList = customerModel.ListCustomers();
+
+        //        // Attaches associated address to customer
+        //        foreach (var customer in customerList)
+        //        {
+        //            Address address = null;
+        //            if (customer.Address_ID != 0)
+        //            {
+        //                address = addressModel.SearchAddress(customer.Address_ID);
+        //            }
+
+        //            // Appends object to address
+        //            customer.Address = address;
+        //        }
+
+        //        // Return the CustomerList
+        //        return View(customerList);
+        //    }
+        //    else
+        //    {
+        //        return Redirect("/403.html");
+        //    }
+        //}
+        
+        // Creates a new customer
+        public int create(String name, int addressID)
+        {
+            // Establishes models
+            CustomerModel customerModel = new CustomerModel();
+
+            // Holds object placeholders
+            Customer newCustomer = new Customer();
+
+            // Stored details for the customer
+            newCustomer.Name = name;
+            newCustomer.Address_ID = addressID;
+                
+            // Creates the customer
+            int customerID = customerModel.CreateCustomer(newCustomer);
+
+            // Return created department to view
+            return customerID;
+        }
+
+        //// Allows modification of customer
+        //public ActionResult EditCustomer()
+        //{
+        //    // Null handling
+        //    if (Session["loggedInState"] == null)
+        //    {
+        //        return Redirect("/403.html");
+        //    }
+
+        //    // If logged in
+        //    bool state = (bool)Session["loggedInState"];
+        //    if (state == true)
+        //    {
+        //        int customerID = int.Parse(Url.RequestContext.RouteData.Values["id"].ToString());
+
+        //        // Creates object placeholders
+        //        Customer customer = new Customer();
+
+        //        // Setup customer edit
+        //        customer.ID = int.Parse(Request.Form["customerID"]);
+        //        customer.Name = Request.Form["name"];
+        //        customer.Address_ID = int.Parse(Request.Form["addressID"]);
+        //        customer.Account_ID = int.Parse(Request.Form["accountID"]);
+
+        //        // Establishes customer model
+        //        CustomerModel customerModel = new CustomerModel();
+
+        //        // Conduct edit
+        //        customerModel.EditCustomer(customer);
+
+        //        // Passes back to the view
+        //        return View();
+        //    }
+        //    else
+        //    {
+        //        // If not logged in
+        //        return Redirect("/login.html");
+        //    }
+        //}
+
+        //// Allows deleting of customer
+        //public ActionResult Delete()
+        //{
+        //    // Null handling
+        //    if (Session["loggedInState"] == null)
+        //    {
+        //        return Redirect("/403.html");
+        //    }
+
+        //    // Checks if logged in
+        //    bool state = (bool)Session["loggedInState"];
+        //    if (state == true)
+        //    {
+        //        int customerID = int.Parse(RouteData.Values["id"].ToString());
+
+        //        // Establishes account model
+        //        CustomerModel customerModel = new CustomerModel();                            
+
+        //        // Deletes the account, and contact from the database using the ID
+        //        customerModel.DeleteCustomer(customerID);
+                
+        //        // TODO: Confirm this is the correct return state
+        //        // Return to the account page
+        //        return Redirect("/..account");
+        //    }
+        //    else
+        //    {
+        //        // If not logged in
+        //        return Redirect("/login.html");
+        //    }
+        //}
+    }
+}
