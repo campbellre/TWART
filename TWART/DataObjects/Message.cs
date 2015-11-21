7aca6a735c31ca4773c6f273b91252eb3c43cf3d
campbellre <campbellre92@outlook.com>
2015-11-21 15:28:42 +0000

    Made Messages.

---
+++ TWART/DataObjects/Message.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TWART.DataObjects
+{
+    public class Message
+    {
+        public int MessageID { get; set; }
+        public String MessageBody { get; set; }
+        public String Name { get; set; }
+        public String Email { get; set; }
+    }
+}
