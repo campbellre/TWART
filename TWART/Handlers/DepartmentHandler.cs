ccafeea6359dace671c7dd0a02cdc9df747f32cd
Budds2k6 <Budds_2k6@hotmail.co.uk>
2015-11-21 23:51:27 +0000

    Hotfix temporary post to satisfy Andrew.

---
+++ TWART/Handlers/DepartmentHandler.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TWART.DataObjects;
+using TWART.Models;
+
+namespace TWART.Handlers
+{
+    public class DepartmentHandler
+    {
+        // Constructor
+        public DepartmentHandler()
+        { }
+
+        // Create new department
+        public int create(int address, String title)
+        {
+            // Establishes models
+            DepartmentModel deptModel = new DepartmentModel();
+
+            // Holds new department details
+            Department newDept = new Department();
+
+            // Stored details for the department
+            newDept.AddressID = address;
+            newDept.Title = title;
+
+            // Creates the department
+            int deptID = deptModel.CreateNewDepartment(newDept);
+
+            // Returns departmentId
+            return deptID;
+        }
+    }
+}
---
+++ TWART/Handlers/DepotHandler.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TWART.DataObjects;
+using TWART.Models;
+
+namespace TWART.Handlers
+{
+    public class DepotHandler
+    {
+        // Constructor
+        public DepotHandler()
+        {}
+
+        // Creates a depot
+        public int create(int addressID, String depotName, int floorSpace)
+        {
+            // Establishes depot model
+            DepotModel depoModel = new DepotModel();
+
+            // Holds new depot
+            Depot newDepot = new Depot();
+
+            // Stored details for the depot
+            newDepot.AddressID = addressID;
+            newDepot.DepotName = depotName;
+            newDepot.FloorSpace = floorSpace;
+
+            // Acquires depotID
+            int depotID = depoModel.CreateDepot(newDepot);
+
+            // Returns depotID
+            return depotID;
+        }
+    }
+}
